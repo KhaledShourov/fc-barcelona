cc8ce233f010db85c972beb069a72ec92c150519
Your Name <you@example.com>
2021-05-11 22:37:26 +0600

    json data add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-import './App.css';
-import Header from './components/Header/Header';
-import Team from './components/Team/Team';
 
 
 function App() {
   return (
     <div>
-     <Header></Header>
-     <Team></Team>
-
+     
     </div>
   );
 }
